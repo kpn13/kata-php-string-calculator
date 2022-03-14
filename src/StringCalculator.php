80c90c5b971f5aa3f4a6ca26a3370a973a165e7b
Karim PINCHON <karim.pinchon@ornikar.com>
2022-03-14 09:37:07 +0100

    refacto: improve readability

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,35 +4,33 @@ namespace App;
 
 class StringCalculator
 {
-    public function __construct(private string $separator = ',')
-    {
-    }
-
     public function add(string $numbersString): string
     {
-        if ($numbersString === '') {
+        if (empty($numbersString)) {
             return '0';
         }
 
-        if (str_contains($numbersString, ',\n')) {
-            return sprintf('Number expected but \'\n\' found at position %d.', strpos($numbersString, ',\n') + 1);
-        }
-
-        if (str_ends_with($numbersString, ',')) {
-            return 'Number expected but EOF found.';
-        }
+        $separator = ',';
 
         if (preg_match('/^\/\/(.*)\\\n/', $numbersString, $matches)) {
-            $this->separator = $matches[1];
+            $separator = $matches[1];
             $numbersString = str_replace($matches[0], '', $numbersString);
         }
 
-        $numbersString = str_replace('\n', $this->separator, $numbersString);
-
-        if (preg_match('/([^0-9\.' . $this->separator . '])/', $numbersString, $matches)){
-            return sprintf('\'%s\' expected but \'%s\' found at position %d.', $this->separator, $matches[0], strpos($numbersString, $matches[0]));
+        if (str_contains($numbersString, $separator . '\n')) {
+            return sprintf('Number expected but \'\n\' found at position %d.', strpos($numbersString, ',\n') + 1);
         }
 
-        return array_sum(explode($this->separator, $numbersString));
+        if (str_ends_with($numbersString, $separator)) {
+            return 'Number expected but EOF found.';
+        }
+
+        $numbersString = str_replace('\n', $separator, $numbersString);
+
+        if (preg_match('/([^0-9\.' . $separator . '])/', $numbersString, $matches)){
+            return sprintf('\'%s\' expected but \'%s\' found at position %d.', $separator, $matches[0], strpos($numbersString, $matches[0]));
+        }
+
+        return array_sum(explode($separator, $numbersString));
     }
 }
